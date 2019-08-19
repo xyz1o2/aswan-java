ed15c48a67e5f4dad49d2a448a7f3cc022e202a6
complone <yuluoxinsheng@gmail.com>
2019-08-19 14:44:52 +0800

    引入shrio,初始化shrio模块

---
+++ aswan-java-menu/src/main/java/com/complone/aswan/controller/MenuController.java
@@ -0,0 +1,17 @@
+package com.complone.aswan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by chengxingyuan on 2019/8/19.
+ */
+@Controller
+@RequestMapping("/menu/admin")
+public class MenuController {
+
+    public String createMenu(){
+        return null;
+    }
+
+}
---
+++ aswan-java-menu/src/main/java/com/complone/aswan/entity/MenuPO.java
@@ -0,0 +1,11 @@
+package com.complone.aswan.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by chengxingyuan on 2019/8/19.
+ */
+public class MenuPO implements Serializable{
+
+
+}
